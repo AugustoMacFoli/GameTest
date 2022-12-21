c130ed971e139b2e591060841629e8c5604bbd69
Augusto Oliveira <augusto.mfo@gmail.com>
2022-12-21 18:56:43 +0000

    Added shoot

--- mainForm.Designer.cs
+++ mainForm.Designer.cs
@@ -32,6 +32,10 @@
             this.floor = new System.Windows.Forms.Panel();
             this.player = new System.Windows.Forms.Panel();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.labelShootCount = new System.Windows.Forms.Label();
+            this.labelActiveBulletCount = new System.Windows.Forms.Label();
+            this.labelShootCountValue = new System.Windows.Forms.Label();
+            this.labelActiveBulletCountValue = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // floor
@@ -45,7 +49,7 @@
             // player
             // 
             this.player.BackColor = System.Drawing.Color.MediumBlue;
-            this.player.Location = new System.Drawing.Point(348, 320);
+            this.player.Location = new System.Drawing.Point(102, 320);
             this.player.Name = "player";
             this.player.Size = new System.Drawing.Size(48, 91);
             this.player.TabIndex = 1;
@@ -55,11 +59,51 @@
             this.gameTimer.Enabled = true;
             this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
             // 
+            // labelShootCount
+            // 
+            this.labelShootCount.AutoSize = true;
+            this.labelShootCount.Location = new System.Drawing.Point(26, 23);
+            this.labelShootCount.Name = "labelShootCount";
+            this.labelShootCount.Size = new System.Drawing.Size(92, 20);
+            this.labelShootCount.TabIndex = 2;
+            this.labelShootCount.Text = "Shoot count:";
+            // 
+            // labelActiveBulletCount
+            // 
+            this.labelActiveBulletCount.AutoSize = true;
+            this.labelActiveBulletCount.Location = new System.Drawing.Point(26, 57);
+            this.labelActiveBulletCount.Name = "labelActiveBulletCount";
+            this.labelActiveBulletCount.Size = new System.Drawing.Size(136, 20);
+            this.labelActiveBulletCount.TabIndex = 3;
+            this.labelActiveBulletCount.Text = "Active Bullet count:";
+            // 
+            // labelShootCountValue
+            // 
+            this.labelShootCountValue.AutoSize = true;
+            this.labelShootCountValue.Location = new System.Drawing.Point(124, 23);
+            this.labelShootCountValue.Name = "labelShootCountValue";
+            this.labelShootCountValue.Size = new System.Drawing.Size(17, 20);
+            this.labelShootCountValue.TabIndex = 4;
+            this.labelShootCountValue.Text = "0";
+            // 
+            // labelActiveBulletCountValue
+            // 
+            this.labelActiveBulletCountValue.AutoSize = true;
+            this.labelActiveBulletCountValue.Location = new System.Drawing.Point(168, 57);
+            this.labelActiveBulletCountValue.Name = "labelActiveBulletCountValue";
+            this.labelActiveBulletCountValue.Size = new System.Drawing.Size(17, 20);
+            this.labelActiveBulletCountValue.TabIndex = 5;
+            this.labelActiveBulletCountValue.Text = "0";
+            // 
             // scenario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelActiveBulletCountValue);
+            this.Controls.Add(this.labelShootCountValue);
+            this.Controls.Add(this.labelActiveBulletCount);
+            this.Controls.Add(this.labelShootCount);
             this.Controls.Add(this.player);
             this.Controls.Add(this.floor);
             this.KeyPreview = true;
@@ -69,6 +113,7 @@
             this.Text = "GameTest";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.scenario_KeyDown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -77,5 +122,9 @@
         private Panel floor;
         private Panel player;
         private System.Windows.Forms.Timer gameTimer;
+        private Label labelShootCount;
+        private Label labelActiveBulletCount;
+        private Label labelShootCountValue;
+        private Label labelActiveBulletCountValue;
     }
 }
--- mainForm.cs
+++ mainForm.cs
@@ -6,34 +6,104 @@ namespace GameTest
         private readonly int playerHorizontalSpeed = 20;
         private readonly int playerJumpSize = 10;
         private readonly int playerJumpSpeed = 10;
+        private readonly int bulletSpeed = 100;
         private Queue<int> playerJump = new Queue<int>();
+        private Queue<Control> trash = new Queue<Control>();
 
         public scenario()
         {
             InitializeComponent();
         }
 
+        private void scenario_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+                jump();
+
+            if (e.KeyCode == Keys.Left)
+                moveLeft();
+
+            if (e.KeyCode == Keys.Right)
+                moveRight();
+
+            if (e.KeyCode == Keys.Enter)
+                shoot();
+        }
+
+        private void moveLeft()
+        {
+            player.Left -= playerHorizontalSpeed;
+        }
+
+        private void moveRight()
+        {
+            player.Left += playerHorizontalSpeed;
+        }
+
+        private void jump()
+        {
+            if (!playerJump.Any() && floor.Bounds.IntersectsWith(player.Bounds))
+            {
+                for (int i = 1; i < playerJumpSize; i++)
+                {
+                    playerJump.Enqueue(playerJumpSpeed);
+                }
+            }
+        }
+
+        private void shoot()
+        {
+            var bullet = new Panel();
+            bullet.Width = 5;
+            bullet.Height = 5;
+            bullet.Top = player.Top + 25;
+            bullet.Left = player.Left + 50;
+            bullet.BackColor = Color.Red;
+            bullet.Tag = "bullet";
+            this.Controls.Add(bullet);
+            this.labelShootCountValue.Text = (Convert.ToInt32(this.labelShootCountValue.Text) + 1).ToString();
+        }
+
         private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            handleJump();
+            handleGravity();
+            handleBullets();
+            handleTrash();
+        }
+
+        private void handleJump()
         {
             if (playerJump.Any())
                 player.Top -= playerJump.Dequeue();
+        }
 
+        private void handleGravity()
+        {
             if (!playerJump.Any() && !floor.Bounds.IntersectsWith(player.Bounds))
                 player.Top += gravity;
         }
 
-        private void scenario_KeyDown(object sender, KeyEventArgs e)
+        private void handleBullets()
         {
-            if (e.KeyCode == Keys.Space)
-                if (!playerJump.Any())
-                    for (int i = 1; i < playerJumpSize; i++)
-                        playerJump.Enqueue(playerJumpSpeed);
+            foreach (Control control in this.Controls)
+            {
+                if (control.Tag == "bullet")
+                    control.Left += bulletSpeed;
 
-            if (e.KeyCode == Keys.Left)
-                player.Left -= playerHorizontalSpeed;
+                if (control.Tag == "bullet" && control.Right >= scenario.ActiveForm.Right)
+                    trash.Enqueue(control);
+            }
+        }
 
-            if (e.KeyCode == Keys.Right)
-                player.Left += playerHorizontalSpeed;
+        private void handleTrash()
+        {
+            while (trash.Any())
+            {
+                this.Controls.Remove(trash.Dequeue());
+            }
+            labelActiveBulletCountValue.Text =
+                this.Controls.OfType<Panel>().Where(x => x.Tag == "bullet").Count().ToString();
         }
     }
 }
